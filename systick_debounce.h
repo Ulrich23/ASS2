6019fba4eb2d109d8fa7aa3ec74b498533ada1a6
Ulrich23 <49520062+Ulrich23@users.noreply.github.com>
2020-02-26 10:39:46 +0100

    Update systick_debounce.h

--- systick_debounce.h
+++ systick_debounce.h
@@ -1,10 +1,6 @@
 #ifndef SYSTICK_DEBOUCE_H_
 #define SYSTICK_DEBOUCE_H_
 
-void systick_debounce();
-
-void systick_debounce990ms();
-
-void systick_debounce1000ms();
+void systick_debounce(INT64S timeval);
 
 #endif /* LIGHT_STATES_H_ */
